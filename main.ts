1924418e024c662c23871474ff092fc4afc361e0
Musarrat khan <161501864+Musarratkha@users.noreply.github.com>
2024-03-20 22:48:35 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+// let num1 =20;
+// num1+=2;
+// console.log(num1)
+
+// let num1= 20;
+// num1+= 22;
+// console.log(num1)
+
+// let num1= 42;
+// num1-= 20;
+// console.log(num1-20)
+
+// let num2= 42;
+// num2-= 20;
+// console.log(num2)
+
+// let num3= 5;
+// num3 *= 3;
+// console.log(num3)
+
+// let num3= 6;
+// num3/= 2;
+// console.log(num3)
